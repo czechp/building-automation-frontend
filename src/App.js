cd3ff122a6712ffc4660bd6173f06ae18d068b5a
czechp <webcoderc@gmail.com>
2022-07-25 21:59:55 +0200

    Set theme provider from styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {ThemeProvider} from "styled-components";
+import theme from "./configuration/theme";
 
 function App() {
     return (<div className="App">
+        <ThemeProvider theme={theme}>
 
+        </ThemeProvider>
     </div>);
 }
 
