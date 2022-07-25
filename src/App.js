06a0806b99e64b4beb075bff0e764a682c2cce9f
czechp <webcoderc@gmail.com>
2022-07-25 22:45:11 +0200

    Set margins for body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
 import {ThemeProvider} from "styled-components";
 import theme from "./configuration/theme";
 import GlobalStyles from "./configuration/globalStyles";
+import ContainerLayout from "./layout/ContainerLayout";
+import TopBarLayout from "./layout/TopBarLayout";
 
 function App() {
     return (<div className="App">
         <ThemeProvider theme={theme}>
         <GlobalStyles />
+            <ContainerLayout>
+                <TopBarLayout/>
+            </ContainerLayout>
         </ThemeProvider>
     </div>);
 }
