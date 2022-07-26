a3ce5b93433266c0d03f9f67f4a5141773d6def1
czechp <webcoderc@gmail.com>
2022-07-26 11:28:53 +0200

    Separator component (horizontal line)

---
+++ src/component/SeparatorCmp.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const SeparatorCmp = () => {
+    return <Separator/>
+}
+
+const Separator = styled.hr`
+    color: ${({theme})=>theme.colors.primary};
+`
+
+export default SeparatorCmp;
