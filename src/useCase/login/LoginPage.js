1a74b335de61e2699b719adbd3948fb2b6ae76f3
czechp <webcoderc@gmail.com>
2022-07-27 23:02:27 +0200

    Login page layout

--- src/useCase/login/LoginPage.js
+++ src/useCase/login/LoginPage.js
@@ -1,7 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
 import PageCmp from "../../component/PageCmp";
+import FormCmp from "../../component/FormCmp";
+import TextInputCmp from "../../component/TextInputCmp";
+import ButtonCmp from "../../component/ButtonCmp";
 
 const LoginPage = () => {
-    return <PageCmp title="Login"></PageCmp>
+    const [login, setLogin] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    return <PageCmp title="Login">
+        <FormCmp title="Login">
+            <TextInputCmp placeholder="Type your login" label="Username:" value={login} onChange={setLogin}
+                          minLength={3}/>
+            <TextInputCmp type="password" placeholder="Type your password" label="Password:" value={password}
+                          onChange={setPassword} minLength={3}/>
+            <ButtonCmp label="Sing in" />
+            <RegisterInfo>If you do not have an account </RegisterInfo>
+            <ButtonCmp label="Register"  />
+        </FormCmp>
+    </PageCmp>
 }
+
+const RegisterInfo = styled.p`
+    font-style: italic;
+`
 
 export default LoginPage;
