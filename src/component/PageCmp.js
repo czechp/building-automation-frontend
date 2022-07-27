36a879c43b9ffe769c7bf2cadc470d1692ca53a8
czechp <webcoderc@gmail.com>
2022-07-27 20:13:27 +0200

    Page component to unify looks of pages

---
+++ src/component/PageCmp.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+import colors from "../configuration/colors";
+import StatementCmp from "./StatementCmp";
+import {createContext, useState} from "react";
+import ButtonCmp from "./ButtonCmp";
+
+export const StatementContext = createContext({});
+
+const PageCmp = ({title, children}) => {
+    const [statement, setStatement] = useState({text: "", error: false});
+
+    const setErrorStatement = (text) => {
+        setStatement({text, error: true});
+    }
+    const setSuccessStatement = (text) => {
+        setStatement({text, error: false});
+    }
+
+    return <Container>
+        <Title>{title}</Title>
+        <hr width="30%" align="left" color={colors.primary}/>
+        <StatementContext.Provider value={{
+            text: statement.text,
+            error: statement.error,
+            setErrorStatement,
+            setSuccessStatement
+        }}>
+            <Content>{children}</Content>
+            <StatementCmp/>
+        </StatementContext.Provider>
+    </Container>
+
+}
+
+const Container = styled.div`
+  width: 100%;
+`
+
+const Title = styled.h2`
+`
+
+const Content = styled.div`
+  margin-top: 150px;
+  display: flex;
+  justify-content: center;
+  width: 60%;
+`
+
+
+export default PageCmp;
