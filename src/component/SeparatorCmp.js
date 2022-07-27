5a65b2c04b65d3cea0aa7b5e51fb408fe07f4ee8
czechp <webcoderc@gmail.com>
2022-07-27 20:11:39 +0200

    Update global styles

--- src/component/SeparatorCmp.js
+++ src/component/SeparatorCmp.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 const SeparatorCmp = () => {
-    return <Separator/>
+    return <Separator />
 }
 
 const Separator = styled.hr`
   color: ${({theme}) => theme.colors.primary};
   background-color: ${({theme}) => theme.colors.primary};
+  size: 1px;
 `
 
 export default SeparatorCmp;
