cea8fcb91fc14c3a2463ad5e555b77a844fcd4c4
czechp <webcoderc@gmail.com>
2022-07-28 23:53:26 +0200

    Send login request to authorize user

--- src/useCase/login/LoginPage.js
+++ src/useCase/login/LoginPage.js
@@ -6,19 +6,40 @@ import FormCmp from "../../component/FormCmp";
 import TextInputCmp from "../../component/TextInputCmp";
 import ButtonCmp from "../../component/ButtonCmp";
 import {StatementContext} from "../../App";
+import {fieldsValidator} from "../../service/validator/fieldsValidator";
+import sendPostRequest from "../../service/http/sendPostRequest";
 
 const LoginPage = () => {
-    const [login, setLogin] = React.useState("");
-    const [password, setPassword] = React.useState("");
+    const MINIMUM_FIELDS_LENGTH = 3;
+    const LOGIN_ENDPOINT = "/api/accounts/login";
 
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const {showErrorInfo, showSuccessInfo} = React.useContext(StatementContext);
+
+    function validateFields() {
+        return [username, password].every(f => fieldsValidator.minimumLength(f, MINIMUM_FIELDS_LENGTH));
+    }
+
+    function sendAuthorizeRequest() {
+        sendPostRequest(LOGIN_ENDPOINT, {username, password})
+            .then((response) => showSuccessInfo("Login successfully"))
+            .catch((error) => console.log(error));
+    }
+
+    function loginOnClick() {
+        if (validateFields())
+            sendAuthorizeRequest();
+        else showErrorInfo("Check correctness of fields");
+    }
 
     return <PageCmp title="Login">
         <FormCmp>
-            <TextInputCmp placeholder="Type your login" label="Username:" value={login} onChange={setLogin}
+            <TextInputCmp placeholder="Type your login" label="Username:" value={username} onChange={setUsername}
                           minLength={3}/>
             <TextInputCmp type="password" placeholder="Type your password" label="Password:" value={password}
                           onChange={setPassword} minLength={3}/>
-            <ButtonCmp label="Sing in" />
+            <ButtonCmp label="Sing in" onClick={loginOnClick}/>
             <RegisterInfo>If you do not have an account </RegisterInfo>
             <ButtonCmp label="Register"/>
         </FormCmp>
