37b2b71b17eb56261addd5e573e891c32369da3f
czechp <webcoderc@gmail.com>
2022-07-29 13:09:40 +0200

    Authorization service

---
+++ src/service/authorization/authorizationService.js
@@ -0,0 +1,35 @@
+import storageService from "../storage/storageService";
+
+const ID = "id";
+const USERNAME = "username";
+const EMAIL = "email";
+const ROLE = "role";
+const AUTHORIZATION_HEADER = "authorizationHeader"
+
+const authorizationService = {
+    storeUserInfo, getUserInfo
+};
+
+function storeUserInfo({id, username, password, email, role}) {
+    storageService.saveValue(ID, id);
+    storageService.saveValue(USERNAME, username);
+    storageService.saveValue(EMAIL, email);
+    storageService.saveValue(ROLE, role);
+    storageService.saveValue(AUTHORIZATION_HEADER, createBasicAuthorizationHeader(username, password));
+}
+
+function createBasicAuthorizationHeader(username, password) {
+    return "BASIC " + window.btoa(`${username}:${password}`);
+}
+
+function getUserInfo() {
+    return {
+        id: storageService.readValue(ID),
+        username: storageService.readValue(USERNAME),
+        email: storageService.readValue(EMAIL),
+        role: storageService.readValue(ROLE),
+        authorizationHeader: storageService.readValue(AUTHORIZATION_HEADER)
+    };
+}
+
+export default authorizationService;
