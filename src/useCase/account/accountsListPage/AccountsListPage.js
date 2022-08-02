273fe85520abf74416763345bc2ca2397d735264
czechp <webcoderc@gmail.com>
2022-08-03 00:12:49 +0200

    Display accounts in table

--- src/useCase/account/accountsListPage/AccountsListPage.js
+++ src/useCase/account/accountsListPage/AccountsListPage.js
@@ -1,9 +1,37 @@
+import React from "react";
+
 import PageCmp from "../../../component/PageCmp";
+import useGetAccountsRequest from "./request/useGetAccountsRequest";
+import {Table, Td, Th, Tr} from "../../../configuration/styledComponents/Table";
 
 const AccountsListPage = () => {
-    return <PageCmp title="Accounts list">
+    const accounts = useGetAccountsRequest();
 
+
+    return <PageCmp title="Accounts list">
+        <Table>
+            <Tr>
+                <Th>Id:</Th>
+                <Th>Username:</Th>
+                <Th>Email:</Th>
+                <Th>Role:</Th>
+                <Th>Creation date:</Th>
+            </Tr>
+            {
+                accounts.map((a,id)=><AccountRow account={a} id={id} />)
+            }
+        </Table>
     </PageCmp>
 }
 
+
+const AccountRow = ({account, id}) => {
+    return <Tr key={`${account.id}-${id}`}>
+        <Td>{account.id}</Td>
+        <Td>{account.username}</Td>
+        <Td>{account.email}</Td>
+        <Td>{account.role}</Td>
+        <Td>{account.creationTimestamp}</Td>
+    </Tr>
+}
 export default AccountsListPage;
