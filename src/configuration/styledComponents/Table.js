65374c1dc2ff5020a1ea61ebc6acaf01d0f3f9a4
czechp <webcoderc@gmail.com>
2022-08-03 00:10:07 +0200

    Table styled component

---
+++ src/configuration/styledComponents/Table.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+  width: 100%;
+  padding: 20px;
+`;
+export const Tr = styled.tr`
+    &:hover{
+      background-color: ${({theme}) => theme.colors.primary};
+      color: ${({theme}) => theme.colors.white};
+      cursor: pointer;
+    }
+`;
+
+export const Th = styled.th`
+  text-align: center;
+  padding: 20px;
+  border-bottom: 2px solid ${({theme}) => theme.colors.primary};
+  border-top: 2px solid ${({theme}) => theme.colors.primary};
+  &:hover{
+    background-color: ${({theme}) => theme.colors.white};
+    color: ${({theme}) => theme.colors.primary};
+    cursor: pointer;
+  }
+
+`;
+export const Td = styled.td`
+  text-align: center;
+  padding: 20px;
+  border-bottom: 2px solid ${({theme}) => theme.colors.primary};
+
+`;
