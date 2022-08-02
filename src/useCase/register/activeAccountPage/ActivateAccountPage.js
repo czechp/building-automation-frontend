2f223afd009f953c10f4455e8d6225c42ffd1aea
czechp <webcoderc@gmail.com>
2022-08-02 21:02:27 +0200

    Page to activation code by email code

--- src/useCase/register/activeAccountPage/ActivateAccountPage.js
+++ src/useCase/register/activeAccountPage/ActivateAccountPage.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styled from "styled-components";
-import {useLocation} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 
 import PageCmp from "../../../component/PageCmp";
 import FormCmp from "../../../component/FormCmp";
 import TextInputCmp from "../../../component/TextInputCmp";
 import ButtonCmp from "../../../component/ButtonCmp";
+import activationCodeFieldValidator from "./validator/activationCodeFieldValidator";
+import {StatementContext} from "../../../App";
+import UUID_LENGTH from "../../../constant/UUID";
+import sendRequestService from "../../../service/http/sendRequestService";
+import httpErrorHandler from "../../../service/http/httpErrorHandler";
 
 const ActivateAccountPage = () => {
     const {state: activationCode} = useLocation();
+
 
     return <PageCmp title="Activate account">
         <DevelopmentSection activationCode={activationCode}/>
@@ -28,16 +34,35 @@ const DevelopmentSection = ({activationCode}) => {
 const ActivationForm = ({activationCode: myActivationCode}) => {
     const ACTIVATION_ENDPOINT = "/api/accounts/email-confirmation";
     const [activationCode, setActivationCode] = React.useState(myActivationCode || "");
+    const navigate = useNavigate();
 
-    function activationBtnOnClick(){
+    const {showErrorInfo, showSuccessInfo} = React.useContext(StatementContext);
 
+    function activationSuccess() {
+         showSuccessInfo("Account activation success. Now wait for admin confirmation.");
+         navigate("/login");
+    }
+
+    function sendActivationRequest() {
+        sendRequestService.patch(ACTIVATION_ENDPOINT, {}, [{token: activationCode}])
+            .then((response) => activationSuccess())
+            .catch((error) => showErrorInfo(httpErrorHandler(error)));
+    }
+
+    function activationBtnOnClick() {
+        if (activationCodeFieldValidator(activationCode))
+            sendActivationRequest();
+        else
+            showErrorInfo("Your activation code has to have 36 characters")
     };
 
     return <FormCmp>
         <TextInputCmp label="Activation code:"
                       value={activationCode}
                       onChange={setActivationCode}
-                      placeholder="Type your activation code from email message"/>
+                      placeholder="Type your activation code from email message"
+                      minLength={UUID_LENGTH}
+        />
         <ButtonCmp label="Activate account" onClick={activationBtnOnClick}/>
     </FormCmp>
 }
