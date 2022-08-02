08dfde7fc183876131623c19f60152144623b584
czechp <webcoderc@gmail.com>
2022-08-02 20:58:40 +0200

    Additional configuration to http requests

--- src/service/http/sendRequestService.js
+++ src/service/http/sendRequestService.js
@@ -1,14 +1,35 @@
 import createAxiosInstance from "../../configuration/http/axiosConfiguration";
 
 
+function createConfig(params = []) {
+    return {
+        params: createParams(params)
+    }
+}
+
+function createParams(params) {
+    const urlParams = new URLSearchParams();
+    params.forEach(p => {
+        const {key, value} = createKeyValuePair(p);
+        urlParams.append(key, value);
+    })
+    return urlParams;
+}
+
+function createKeyValuePair(param) {
+    const key = Object.keys(param)[0];
+    const value = param[key];
+    return {key, value};
+}
+
 const sendRequestService = {
-    post: function (endpoint, body = {}) {
+    post: function (endpoint, body = {}, params=[]) {
         return createAxiosInstance()
-            .post(endpoint, body);
+            .post(endpoint, body, createConfig(params));
     },
-    patch:  function (endpoint, body = {}) {
+    patch: function (endpoint, body = {}, params = []) {
         return createAxiosInstance()
-            .patch(endpoint, body);
+            .patch(endpoint, body, createConfig(params));
     }
 }
 
