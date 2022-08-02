c9fc6cb04c9188bd0742855f74eb75f40e40efdd
czechp <webcoderc@gmail.com>
2022-08-02 22:39:39 +0200

    Account list page backbone

---
+++ src/useCase/account/accountsListPage/AccountsListPage.js
@@ -0,0 +1,9 @@
+import PageCmp from "../../../component/PageCmp";
+
+const AccountsListPage = () => {
+    return <PageCmp title="Accounts list">
+
+    </PageCmp>
+}
+
+export default AccountsListPage;
