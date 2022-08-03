576d8d6ca60fb7d42372a689e042cd3f12ca9b42
czechp <webcoderc@gmail.com>
2022-08-04 00:12:54 +0200

    Create hooks directory and move hooks into it

--- src/useCase/account/accountsListPage/hook/useGetAccountsRequest.js
+++ src/useCase/account/accountsListPage/hook/useGetAccountsRequest.js
@@ -1,18 +1,19 @@
 import React, {useEffect} from "react";
-import sendRequestService from "../../../../service/http/sendRequestService";
-import {StatementContext} from "../../../../App";
-import httpErrorHandler from "../../../../service/http/httpErrorHandler";
+import sendRequestService from "../../../service/http/sendRequestService";
+import {StatementContext} from "../../../App";
+import httpErrorHandler from "../../../service/http/httpErrorHandler";
 
-function useGetAccountsRequest() {
+function useGetAccountsRequest(sort) {
     const ACCOUNTS_ENDPOINT = "/api/accounts";
     const [accounts, setAccounts] = React.useState([]);
     const {showErrorInfo} = React.useContext(StatementContext);
 
     useEffect(function () {
+        console.log("Invoked")
         sendRequestService.get(ACCOUNTS_ENDPOINT)
             .then((response) => setAccounts(response.data))
             .catch((error) => showErrorInfo(httpErrorHandler(error)));
-    }, []);
+    }, [sort]);
 
     return accounts;
 }
