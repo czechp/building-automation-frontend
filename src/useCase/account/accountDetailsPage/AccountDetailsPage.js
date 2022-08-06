d650a2545a29d17a58df913ce022917455ff0306
czechp <webcoderc@gmail.com>
2022-08-06 23:28:12 +0200

    Invoked cmp to activation account in account details page

--- src/useCase/account/accountDetailsPage/AccountDetailsPage.js
+++ src/useCase/account/accountDetailsPage/AccountDetailsPage.js
@@ -3,17 +3,22 @@ import {useLocation} from "react-router-dom";
 import PageCmp from "../../../component/PageCmp";
 import GetRequestService from "../../../service/http/getRequestService";
 import AccountDetailsInfoCmp from "./component/AccountDetailsInfoCmp";
+import AccountAdminActivationCmp from "./component/AccountAdminActivationCmp";
 
 const AccountDetailsPage = () => {
     const ACCOUNT_DETAILS_ENDPOINT = "/api/accounts";
     const getRequestService = new GetRequestService();
     const {state: accountId} = useLocation();
-    const account = getRequestService.getObject(`${ACCOUNT_DETAILS_ENDPOINT}/${accountId}`);
+    const {
+        object: account,
+        reloadObject: reloadAccount
+    } = getRequestService.getObject(`${ACCOUNT_DETAILS_ENDPOINT}/${accountId}`);
 
     return <PageCmp title="Account details" loaded={account}>
         {
             account && <>
                 <AccountDetailsInfoCmp account={account}/>
+                <AccountAdminActivationCmp accountId={account.id} reloadAccount={reloadAccount}/>
             </>
         }
     </PageCmp>
--- src/useCase/account/accountDetailsPage/component/AccountDetailsInfoCmp.js
+++ src/useCase/account/accountDetailsPage/component/AccountDetailsInfoCmp.js
@@ -7,12 +7,18 @@ const AccountDetailsInfoCmp = ({account}) => {
     const accountRoleConverter = new AccountRoleConverter();
     const dateConverter = new DateConverter();
 
+    function createConfirmationText(status) {
+        return status ? "Yes" : "No";
+    }
+
     const accountInfo = [
         {label: "Id:", value: account.id},
         {label: "Username:", value: account.username},
         {label: "Email:", value: account.email},
         {label: "Role:", value: accountRoleConverter.toText(account.role)},
-        {label: "Creation date:", value: dateConverter.toFullDateTime(account.creationTimestamp)}
+        {label: "Creation date:", value: dateConverter.toFullDateTime(account.creationTimestamp)},
+        {label: "Email confirmed:", value: createConfirmationText(account.emailConfirmed)},
+        {label: "Admin confirmed:", value: createConfirmationText(account.adminActivated)}
     ]
     return <Container>
         <InfoCardCmp data={accountInfo}/>
