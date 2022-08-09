766452a75c49eb286bf02ae1c554c672dae8bcc9
czechp <webcoderc@gmail.com>
2022-08-09 15:40:01 +0200

    Component to adding a new location

--- src/useCase/location/locationListPage/LocationListPage.js
+++ src/useCase/location/locationListPage/LocationListPage.js
@@ -1,16 +1,18 @@
 import PageCmp from "../../../component/PageCmp";
 import GetRequestService from "../../../service/http/getRequestService";
 import LocationsListCmp from "./component/LocationsListCmp";
+import LocationAddCmp from "./component/LocationAddCmp";
 
 const LocationListPage = () => {
     const LOCATIONS_ENDPOINT = "/api/locations/account";
     const getRequestService = new GetRequestService();
-    const {objects: locations, setSortingField} = getRequestService.getObjectsArray(LOCATIONS_ENDPOINT);
+    const {objects: locations, setSortingField, reloadRequest} = getRequestService.getObjectsArray(LOCATIONS_ENDPOINT);
 
 
     return <PageCmp title="Locations" loaded={locations}>
         {
             locations && <>
+                <LocationAddCmp reloadLocations={reloadRequest} />
                 <LocationsListCmp locations={locations} setSortingField={setSortingField} />
             </>
         }
