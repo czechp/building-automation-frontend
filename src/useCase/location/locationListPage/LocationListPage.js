35c2e34a2e83cdfeb7913991d57779c8c750fcae
czechp <webcoderc@gmail.com>
2022-08-09 12:46:46 +0200

    Get locations from backend and invoke locations list component

--- src/useCase/location/locationListPage/LocationListPage.js
+++ src/useCase/location/locationListPage/LocationListPage.js
@@ -1,7 +1,20 @@
+import PageCmp from "../../../component/PageCmp";
+import GetRequestService from "../../../service/http/getRequestService";
+import LocationsListCmp from "./component/LocationsListCmp";
+
 const LocationListPage = () => {
-    return <div>
-        Location page
-    </div>
+    const LOCATIONS_ENDPOINT = "/api/locations/account";
+    const getRequestService = new GetRequestService();
+    const {objects: locations, setSortingField} = getRequestService.getObjectsArray(LOCATIONS_ENDPOINT);
+
+
+    return <PageCmp title="Locations" loaded={locations}>
+        {
+            locations && <>
+                <LocationsListCmp locations={locations} setSortingField={setSortingField} />
+            </>
+        }
+    </PageCmp>
 }
 
 export default LocationListPage;
