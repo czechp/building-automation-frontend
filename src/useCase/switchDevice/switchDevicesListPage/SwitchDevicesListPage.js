34e12cc1c0bbf037702a7fb7efe5665209e7c0c2
czechp <webcoderc@gmail.com>
2022-08-10 15:51:21 +0200

    Switch device list page backbone

---
+++ src/useCase/switchDevice/switchDevicesListPage/SwitchDevicesListPage.js
@@ -0,0 +1,9 @@
+import PageCmp from "../../../component/PageCmp";
+
+const SwitchDevicesListPage = ()=>{
+    return <PageCmp title="Switch devices">
+
+    </PageCmp>
+}
+
+export default SwitchDevicesListPage;
