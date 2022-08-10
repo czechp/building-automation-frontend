da4ac2fc6b4375502fab24b20b09cf3defd44805
czechp <webcoderc@gmail.com>
2022-08-10 20:31:58 +0200

    Get switch devices from backend and invoke cmp to display it

--- src/useCase/switchDevice/switchDevicesListPage/SwitchDevicesListPage.js
+++ src/useCase/switchDevice/switchDevicesListPage/SwitchDevicesListPage.js
@@ -1,8 +1,23 @@
 import PageCmp from "../../../component/PageCmp";
+import GetRequestService from "../../../service/http/getRequestService";
+import SwitchDevicesListCmp from "./component/SwitchDevicesListCmp";
 
-const SwitchDevicesListPage = ()=>{
-    return <PageCmp title="Switch devices">
+const SwitchDevicesListPage = () => {
+    const SWITCH_DEVICE_ENDPOINT = "/api/switch-devices/account";
 
+    const getRequestService = new GetRequestService();
+    const {
+        objects: switchDevices,
+        setSortingField,
+        reloadRequest: reloadSwitchDevices
+    } = getRequestService.getObjectsArray(SWITCH_DEVICE_ENDPOINT);
+
+    return <PageCmp title="Switch devices" loaded={switchDevices}>
+        {
+            switchDevices && <>
+                <SwitchDevicesListCmp switchDevices={switchDevices} setSortingField={setSortingField} />
+            </>
+        }
     </PageCmp>
 }
 
