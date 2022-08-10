f33de8188467edf126e44ded8ebc29397a330932
czechp <webcoderc@gmail.com>
2022-08-10 20:33:13 +0200

    Component to dispaly switch devices in table form

---
+++ src/useCase/switchDevice/switchDevicesListPage/component/SwitchDevicesListCmp.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+import {Table, Tbody, Td, Th, Thead, Tr} from "../../../../configuration/styledComponents/Table";
+import switchDeviceStateConverter from "../converter/switchDeviceStateConverter";
+import DateConverter from "../../../../service/converter/dateConverter";
+import InfoFrame from "../../../../configuration/styledComponents/InfoFrame";
+import colors from "../../../../configuration/style/colors";
+import ColorExplanationCmp from "../../../../component/ColorExplanationCmp";
+
+const SwitchDevicesListCmp = ({switchDevices, setSortingField}) => {
+
+    const colorExplanation = [
+        {label: "ON", color: colors.success},
+        {label: "OFF", color: colors.off},
+        {label: "ERROR", color: colors.danger},
+    ];
+
+    return <Container>
+        <ColorExplanationCmp data={colorExplanation}/>
+        <Table>
+            <Thead>
+                <Tr>
+                    <Th onClick={() => setSortingField("id")}>Id:</Th>
+                    <Th onClick={() => setSortingField("name")}>Name:</Th>
+                    <Th onClick={() => setSortingField("locationSimpleEntity_name")}>Location:</Th>
+                    <Th onClick={() => setSortingField("expectedState")}>Expected state:</Th>
+                    <Th onClick={() => setSortingField("state")}>Current state:</Th>
+                    <Th onClick={() => setSortingField("deviceError")}>Device error:</Th>
+                    <Th onClick={() => setSortingField("creationTimestamp")}>Creation date:</Th>
+                </Tr>
+            </Thead>
+            <Tbody>
+                {switchDevices.map((device, index) => <SwitchDeviceRow key={`${device.id}-${index}`}
+                                                                       switchDevice={device}/>)}
+            </Tbody>
+        </Table>
+        {switchDevices.length === 0 && <InfoFrame>Currently you have no locations. <br/> Add a new.</InfoFrame>}
+    </Container>
+}
+
+const SwitchDeviceRow = ({switchDevice}) => {
+    const dateConverter = new DateConverter();
+
+    function determineStateColor() {
+        if (switchDevice.deviceError)
+            return colors.danger;
+        else if (switchDevice.state)
+            return colors.success;
+        else
+            return colors.off;
+    }
+
+    return <TrDevice state={determineStateColor()}>
+        <Td>{switchDevice.id}</Td>
+        <Td>{switchDevice.name}</Td>
+        <Td>{switchDevice.location.name}</Td>
+        <Td>{switchDeviceStateConverter(switchDevice.expectedState)}</Td>
+        <Td>{switchDeviceStateConverter(switchDevice.state)}</Td>
+        <Td>{switchDevice.deviceError ? "YES" : "NO"}</Td>
+        <Td>{dateConverter.toFullDateTime(switchDevice.creationTimestamp)}</Td>
+    </TrDevice>
+}
+
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+
+const TrDevice = styled(Tr)`
+  border-left: 20px solid ${({state}) => state};
+`;
+
+export default SwitchDevicesListCmp;
