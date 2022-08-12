172279c8f4c70d54e86214b6c600cfc18028c8d5
czechp <webcoderc@gmail.com>
2022-08-12 23:29:07 +0200

    Added events tile in home page

--- src/useCase/home/HomePage.js
+++ src/useCase/home/HomePage.js
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import {faHouseSignal, faLocationCrosshairs, faPowerOff, faUser} from "@fortawesome/free-solid-svg-icons";
+import {
+    faCalendarCheck,
+    faCalendarXmark,
+    faHouseSignal,
+    faLocationCrosshairs,
+    faPowerOff,
+    faUser
+} from "@fortawesome/free-solid-svg-icons";
 
 
 import PageCmp from "../../component/PageCmp";
@@ -19,6 +26,7 @@ const HomePage = () => {
             <TileCmp path="/" title={"Home"} icon={faHouseSignal}/>
             <TileCmp path="/locations" title={"Locations"} icon={faLocationCrosshairs}/>
             <TileCmp path="/switch-devices" title={"Switch Devices"} icon={faPowerOff}/>
+            <TileCmp path="/events" title={"Events"} icon={faCalendarCheck}/>
             <TileCmp path="/accounts" title={"Accounts"} icon={faUser}/>
         </Container>
     </PageCmp>
@@ -42,12 +50,14 @@ const Container = styled.div`
 const Tile = styled.div`
   border: 2px solid ${({theme}) => theme.colors.primary};
   border-radius: 10px;
-  width: 45%;
-  height: 250px;
+  width: 15%;
+  height: 25vh;
   margin: 1%;
   padding: 1%;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
   &:hover {
     cursor: pointer;
@@ -56,6 +66,8 @@ const Tile = styled.div`
   }
 `;
 
-const TileHeader = styled.h3``;
+const TileHeader = styled.h3`
+    margin: 0 0 2rem 0;
+`;
 
 export default HomePage;
